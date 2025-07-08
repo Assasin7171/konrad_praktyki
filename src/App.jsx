fbca377de908bff2b9650284bc05d58a960775f3
Konrad Bojanecki <KBojanecki@gmail.com>
2025-07-08 14:42:35 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from "./components/Header.jsx";
+
+function App() {
+
+  return (
+    <div className="container-fluid my-5 bg-white">
+        <Header/>
+    </div>
+  )
+}
+
+export default App
